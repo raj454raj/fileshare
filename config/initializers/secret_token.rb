97b5fcb53850e2e2962a94b0811a0f0375a59ddd
raj454raj <raj454raj@gmail.com>
2016-05-17 18:52:43 +0530

    File upload download without auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileshare::Application.config.secret_token = '594c193b6e76ec101817dac2793f8a4dfd80120dbd43b15fc6c9337cec93ffa4020ffed3a48330deb24d81819b39476caa0d2ca67bd7eaaecfc98b4002bd0f8c'
